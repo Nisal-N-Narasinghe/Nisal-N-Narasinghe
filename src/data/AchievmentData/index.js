134bc3408446aa59f6d788507701a1a504b32c76
Nisal <it21259302@my.sliit.lk>
2024-04-13 13:48:20 +0530

    achievments

---
+++ src/data/AchievmentData/index.js
@@ -0,0 +1,65 @@
+const achievementsData = [
+  {
+    title1: "MongoDB for SQL",
+    title2: "Professionals",
+    url: "https://learn.mongodb.com",
+    imageUrl: "images/Certifications/MongoDB-for-SQL-Professionals.png",
+    altText: "MongoDB for SQL",
+    category: "Certifications",
+  },
+  {
+    title1: "MongoDB for SQL",
+    title2: "Professionals",
+    url: "https://www.coursera.org/account/accomplishments/certificate/7P8ZQ6JZM4XK",
+    imageUrl: "images/Certifications/MongoDB-for-SQL-Professionals.png",
+    altText: "MongoDB for SQL",
+    category: "Certifications",
+  },
+  {
+    title1: "MongoDB for SQL",
+    title2: "Professionals",
+    url: "https://www.coursera.org/account/accomplishments/certificate/7P8ZQ6JZM4XK",
+    imageUrl: "images/Certifications/MongoDB-for-SQL-Professionals.png",
+    altText: "MongoDB for SQL",
+    category: "Certifications",
+  },
+  {
+    title1: "MongoDB for SQL",
+    title2: "Professionals",
+    url: "https://www.coursera.org/account/accomplishments/certificate/7P8ZQ6JZM4XK",
+    imageUrl: "images/Certifications/MongoDB-for-SQL-Professionals.png",
+    altText: "MongoDB for SQL",
+    category: "Certifications",
+  },
+
+  {
+    title1: "Pull Shark Badge",
+    url: "https://github.com/Nisal-N-Narasinghe?tab=achievements",
+    imageUrl: "images/Github/pull-shark-badge.png",
+    altText: "Pull Shark Badge",
+    category: "Github Achievements",
+  },
+  {
+    title1: "Galaxy Brain Badge",
+    url: "https://github.com/Nisal-N-Narasinghe?tab=achievements",
+    imageUrl: "images/Github/galaxy-brain-badge.png",
+    altText: "Galaxy Brain Badge",
+    category: "Github Achievements",
+  },
+  {
+    title1: "YOLO Badge",
+    url: "https://github.com/Nisal-N-Narasinghe?tab=achievements",
+    imageUrl: "images/Github/yolo-badge.png",
+    altText: "YOLO Badge",
+    category: "Github Achievements",
+  },
+  {
+    title1: "Quick Draw Badge",
+    url: "https://github.com/Nisal-N-Narasinghe?tab=achievements",
+    imageUrl: "images/Github/quickdraw-badge.png",
+    altText: "Quick Draw Badge",
+    category: "Github Achievements",
+  },
+];
+
+export default achievementsData;
--- src/views/Achievement/index.jsx
+++ src/views/Achievement/index.jsx
@@ -1,21 +1,74 @@
+import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import "../../styles/Achievement.css";
+import achievementsData from "../../data/AchievmentData";
 
 const Achievements = () => {
   return (
-    <div>
+    <div className='bg-black'>
       <Header />
-      <div className='text-white text-center   mt-40 mb-16'>
-        <img
-          src='UnderConstruction.png'
-          alt='Under Construction'
-          className='mx-auto spinning-image'
-        />
-        <h1 className='text-white text-center mb-40 '>Under Construction</h1>
+
+      <h2 className='text-white text-center text-4xl font-extrabold mt-16 certification-img-incoming'>
+        Certifications
+      </h2>
+
+      <div className='flex justify-center mt-16 mb-16'>
+        <div className='flex items-center justify-around max-w-4xl w-full certification-img-incoming'>
+          {achievementsData
+            .filter((item) => item.category === "Certifications")
+            .map((certification, index) => (
+              <a
+                key={index}
+                href={certification.url}
+                target='_blank'
+                rel='noreferrer'>
+                <img
+                  src={certification.imageUrl}
+                  alt={certification.altText}
+                  className='w-48 h-48 rounded-md certification-img '
+                />
+                <p className='text-white text-center text-sm font-extrabold mt-4 '>
+                  {certification.title1}
+                </p>
+                <p className='text-white text-center text-sm font-extrabold  '>
+                  {certification.title2}
+                </p>
+              </a>
+            ))}
+        </div>
       </div>
+
+      <h2 className='text-white text-center text-4xl font-extrabold mt-16 github-img '>
+        Github Achievements
+      </h2>
+
+      <div className='flex justify-center mt-16 mb-16 github-img'>
+        <div className='flex items-center justify-around max-w-4xl w-full'>
+          {achievementsData
+            .filter((item) => item.category === "Github Achievements")
+            .map((achievement, index) => (
+              <a
+                key={index}
+                href={achievement.url}
+                target='_blank'
+                rel='noreferrer'>
+                <img
+                  src={achievement.imageUrl}
+                  alt={achievement.altText}
+                  className='w-48 h-48 '
+                />
+                <p className='text-white text-center text-sm font-extrabold mt-4 '>
+                  {achievement.title1}
+                </p>
+              </a>
+            ))}
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
 };
+
 export default Achievements;
